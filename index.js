3ee630beae0d23247d047cc97c19cf467e3619c9
mary <Mariashee001@gmail.com>
2024-03-28 10:46:46 +0300

    complete the dom modifying element

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+document.querySelector('.main').appendChild(newHeader);
